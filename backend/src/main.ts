22c845b99274785b515ccf0c8d864a238adb96c6
Nicolás Doyhenart <nidoyhe@gmail.com>
2026-06-18 09:50:09 -0300

    Habilitar CORS pruebas y post funcionamiento Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   // Validaciones globales para nuestros DTOs
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
