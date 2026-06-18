70171fde6e4878f9296b60ea96604a38a2a64e7e
Nicolás Doyhenart <nidoyhe@gmail.com>
2026-06-18 11:57:50 -0300

    Cambio apiUrl

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/auth';
+  private apiUrl = 'https://socialnetwork-tp2-backend.onrender.com/';
 
   constructor(private http: HttpClient) {}
 
